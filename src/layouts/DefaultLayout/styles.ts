4a9bee48f3ecbf6064a867734690a101b5bd3f20
feliperbdantas <feliperbdantas@outlook.com>
2024-05-13 21:45:10 -0300

    responsiveness alignment

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,14 +1,44 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  max-width: 74rem;
+  max-width: 60rem;
   height: calc(100vh - 10rem);
   margin: 5rem auto;
-  padding: 2.5rem;
+  padding: 1.5rem;
 
   background: ${(props) => props.theme['gray-800']};
   border-radius: 8px;
 
   display: flex;
   flex-direction: column;
+
+  @media (max-width: 1440px) {
+    padding: 2.5rem;
+    max-width: 60rem;
+    height: calc(100vh - 6rem);
+  }
+
+  @media (max-width: 1024px) {
+    padding: 2rem;
+    max-width: 44rem;
+    height: calc(100vh - 8rem);
+  }
+
+  @media (max-width: 768px) {
+    padding: 1.5rem;
+    max-width: 28rem;
+    height: calc(100vh - 6rem);
+  }
+
+  @media (max-width: 425px) {
+    padding: 1rem;
+    max-width: 20rem;
+    height: calc(100vh - 4rem);
+  }
+
+  @media (max-width: 320px) {
+    padding: 0.5rem;
+    max-width: 20rem;
+    height: calc(100vh - 2rem);
+  }
 `
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -12,7 +12,27 @@ export const HomeContainer = styled.main`
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 3.5rem;
+    gap: 3rem;
+
+    @media (max-width: 1440px) {
+      gap: 3rem;
+    }
+
+    @media (max-width: 1024px) {
+      gap: 2.5rem;
+    }
+
+    @media (max-width: 768px) {
+      gap: 2rem;
+    }
+
+    @media (max-width: 425px) {
+      gap: 1.5rem;
+    }
+
+    @media (max-width: 320px) {
+      gap: 1rem;
+    }
   }
 `
 
@@ -23,18 +43,18 @@ export const FormContainer = styled.div`
   justify-content: center;
   gap: 0.5rem;
   color: ${(props) => props.theme['gray-100']};
-  font-size: 1.125rem;
+  font-size: 1rem;
   font-weight: bold;
   flex-wrap: wrap;
 `
 
 const BaseInput = styled.input`
   background: transparent;
-  height: 2.5rem;
+  height: 2rem;
   border: 0;
   border-bottom: 2px solid ${(props) => props.theme['gray-500']};
   font-weight: bold;
-  font-size: 1.125rem; // inherit
+  font-size: 1rem; // inherit
   padding: 0 0.5rem;
   color: ${(props) => props.theme['gray-100']};
 
@@ -46,64 +66,181 @@ const BaseInput = styled.input`
   &::placeholder {
     color: ${(props) => props.theme['gray-500']};
   }
+
+  @media (max-width: 1440px) {
+    height: 2rem;
+    font-size: 1rem;
+  }
+
+  @media (max-width: 1024px) {
+    height: 1.75rem;
+    font-size: 0.875rem;
+  }
+
+  @media (max-width: 768px) {
+    height: 1.5rem;
+    font-size: 0.75rem;
+  }
+
+  @media (max-width: 425px) {
+    height: 1.25rem;
+    font-size: 0.625rem;
+  }
+
+  @media (max-width: 320px) {
+    height: 1rem;
+    font-size: 0.5rem;
+  }
 `
 
 export const TaskInput = styled(BaseInput)`
-  flex: 1;
-  /* background: ${(props) => props.theme['gray-500']};
-  padding: 1rem;
-  border-radius: 8px;
-  border: 0;
-  color: ${(props) => props.theme['gray-100']};
-  &::placeholder {
-    color: ${(props) => props.theme['gray-300']};
-  } */
+  width: 17rem;
+
+  @media (max-width: 1440px) {
+    width: 17rem;
+  }
+
+  @media (max-width: 1024px) {
+    width: 15rem;
+  }
+
+  @media (max-width: 768px) {
+    width: 13rem;
+  }
+
+  @media (max-width: 425px) {
+    width: 11rem;
+  }
+
+  @media (max-width: 320px) {
+    width: 9rem;
+  }
 `
 
 export const MinutesAmountInput = styled(BaseInput)`
-  width: 4rem;
-  /* text-align: center;
+  width: 2.5rem;
 
-  background: ${(props) => props.theme['gray-500']};
-  padding: 1rem;
-  border-radius: 8px;
-  border: 0;
-  color: ${(props) => props.theme['gray-100']};
-  &::placeholder {
-    color: ${(props) => props.theme['gray-300']};
-  } */
+  @media (max-width: 1440px) {
+    width: 3.5rem;
+  }
+
+  @media (max-width: 1024px) {
+    width: 2.25rem;
+  }
+
+  @media (max-width: 768px) {
+    width: 2rem;
+  }
+
+  @media (max-width: 425px) {
+    width: 1.75rem;
+  }
+
+  @media (max-width: 320px) {
+    width: 1.5rem;
+  }
 `
 
 export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
-  font-size: 10rem;
-  line-height: 8rem;
+  font-size: 9rem;
+  line-height: 7rem;
   color: ${(props) => props.theme['gray-100']};
 
   display: flex;
-  gap: 1rem;
+  gap: 1.5rem;
+
+  @media (max-width: 1440px) {
+    font-size: 9rem;
+    line-height: 7rem;
+  }
+
+  @media (max-width: 1024px) {
+    font-size: 7.5rem;
+    line-height: 5rem;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 7rem;
+    line-height: 4rem;
+  }
+
+  @media (max-width: 425px) {
+    font-size: 6.5rem;
+    line-height: 3rem;
+  }
+
+  @media (max-width: 320px) {
+    font-size: 6rem;
+    line-height: 2rem;
+  }
 
   span {
     background: ${(props) => props.theme['gray-700']};
-    padding: 2rem 1rem;
+    padding: 1.5rem 0.5rem;
     border-radius: 8px;
+
+    @media (max-width: 1440px) {
+      padding: 1.5rem 0.5rem;
+    }
+
+    @media (max-width: 1024px) {
+      padding: 1.25rem 0.25rem;
+    }
+
+    @media (max-width: 768px) {
+      padding: 1rem 0.25rem;
+    }
+
+    @media (max-width: 425px) {
+      padding: 0.75rem 0.25rem;
+    }
+
+    @media (max-width: 320px) {
+      padding: 0.5rem 0.25rem;
+    }
   }
 `
 
 export const Separator = styled.div`
-  padding: 2rem 0;
+  padding: 1.5rem 0;
   color: ${(props) => props.theme['green-500']};
 
-  width: 4rem;
+  width: 3.5rem;
   overflow: hidden;
   display: flex;
   justify-content: center;
+
+  @media (max-width: 1440px) {
+    padding: 1.5rem 0;
+    width: 3.5rem;
+  }
+
+  @media (max-width: 1024px) {
+    padding: 1.25rem 0;
+    width: 3rem;
+  }
+
+  @media (max-width: 768px) {
+    padding: 1rem 0;
+    width: 2.5rem;
+  }
+
+  @media (max-width: 425px) {
+    padding: 0.75rem 0;
+    width: 2rem;
+  }
+
+  @media (max-width: 320px) {
+    padding: 0.5rem 0;
+    width: 1.5rem;
+  }
 `
 
 export const StartCountdownButton = styled.button`
   width: 100%;
   border: 0;
-  padding: 1rem;
+  padding: 0.8rem;
   border-radius: 8px;
 
   display: flex;
@@ -125,5 +262,25 @@ export const StartCountdownButton = styled.button`
 
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
+  }
+
+  @media (max-width: 1440px) {
+    padding: 0.8rem;
+  }
+
+  @media (max-width: 1024px) {
+    padding: 0.6rem;
+  }
+
+  @media (max-width: 768px) {
+    padding: 0.5rem;
+  }
+
+  @media (max-width: 425px) {
+    padding: 0.4rem;
+  }
+
+  @media (max-width: 320px) {
+    padding: 0.3rem;
   }
 `
