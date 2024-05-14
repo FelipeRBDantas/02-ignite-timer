71c189a871928429f05e70d4276464d81c921662
feliperbdantas <feliperbdantas@outlook.com>
2024-05-13 22:50:16 -0300

    History page

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -61,8 +61,31 @@ export const HistoryList = styled.div`
   }
 `
 
-export const Status = styled.span`
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
   display: flex;
   align-items: center;
   gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+    background-color: ${(props) =>
+      props.theme[STATUS_COLORS[props.statusColor]]};
+  }
+
+  color: ${(props) => props.theme['gray-300']};
+  font-size: 0.875rem;
+  line-height: 1.6;
 `
